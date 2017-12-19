8f14d593dbc4e3b2e30898333b771f050a235601
moximoxi3529 <moximoxi3529@163.com>
2017-12-19 10:50:27 +0800

    忘记密码 弹出框 注册

--- js/mine.js
+++ js/mine.js
@@ -1,6 +1,8 @@
 ;(function () {
   $(document).on('click', '.memenu>li', function () {
     $(this).addClass('active').siblings().removeClass('active')
-    
+  })
+  $(document).on('click', '.login', function () {
+    $('.popover').toggle()
   })
 })();
