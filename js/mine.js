b929e6afda6f2149d3aff728b9a6757c4b7febb2
moximoxi3529 <moximoxi3529@163.com>
2017-12-26 12:17:36 +0800

    收藏、联系我们

--- js/mine.js
+++ js/mine.js
@@ -11,6 +11,9 @@
   $(document).on('click', '.item-title li', function () {
     $(this).addClass('active').siblings().removeClass('active')
   })
+  $(document).on('click', '.collecPage .lists li .delete', function () {
+    $(this).parent().parent().remove();
+  })
   // $(document).on('click', '.logo :before', function () {
   //   console.log(111)
   // })
