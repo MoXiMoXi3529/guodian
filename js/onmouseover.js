136c2d2f6abde4f2932e61f090a8fcb6c3c66c8e
moximoxi3529 <moximoxi3529@163.com>
2017-12-28 22:28:51 +0800

    详情页修改

--- js/onmouseover.js
+++ js/onmouseover.js
@@ -1,41 +1,15 @@
 ;(function () {
-  $(document).on('click', '.memenu>li', function () {
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-  $(document).on('click', '.login', function () {
-    $('.popover').toggle()
-  })
-  $(document).on('click', '.more', function () {
-    $('.listsPage .all-title').toggle('500')
-  })
-  $(document).on('click', '.item-title li', function () {
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-  $(document).on('click', '.collecPage .lists li .delete', function () {
-    $(this).parent().parent().remove();
-  })
-  // $(document).on('click', '.logo :before', function () {
-  //   console.log(111)
-  // })
-  var bigImg = document.querySelector('.big-img')
-  var ul = document.querySelector('.small-img')
-  var images = ul.querySelectorAll('img')
-  for (var i = 0; i < images.length; i++) {
-    images[i].onclick = function () {
-      bigImg.src = 'image/0' + this.alt + 'big.jpg'
-    }
-  }
-
-  var box = document.querySelector(".flexslider");
-  var mask = document.querySelector(".mask");
-  var hoverImg = document.querySelector('.hover-img');
-  var img = hoverImg.children[0];
+  var box = document.getElementById("box");
+  var small = box.children[0];
+  var mask = small.children[1];
+  var big = box.children[1];
+  var img = big.children[0];
   //1.鼠标移入小盒子时，显示黄盒子和大盒子
-  bigImg.onmouseover = function () {
+  small.onmouseover = function () {
     mask.style.display = "block";
-    hoverImg.style.display = "block";
+    big.style.display = "block";
     //2.鼠标在小盒子内移动时，黄盒子跟着移动
-    bigImg.onmousemove = function (e) {
+    small.onmousemove = function (e) {
       //3.获取鼠标当前位置与鼠标在小盒子内的差
       var e = e || window.event;
       //获取鼠标在小盒子里的坐标位置
@@ -57,7 +31,7 @@
       //5.黄盒子显示的区域与大盒子保持一致
       //大图与黄盒子的比例  或者是大盒子与小盒子的比例
       //大图在大盒子里移动
-      var bili = hoverImg.offsetWidth / mask.offsetWidth;
+      var bili = big.offsetWidth / mask.offsetWidth;
       //大的图片移动，不是大盒子移动 大图移动方向与黄盒子的移动方向是相反的
       img.style.marginLeft = -bili * x + "px";
       img.style.marginTop = -bili * y + "px";
@@ -66,7 +40,7 @@
   //鼠标移出盒子，黄盒子和大盒子隐藏
   box.onmouseout = function () {
       mask.style.display = "none";
-      hoverImg.style.display = "none";
+      big.style.display = "none";
   }
   function getPageX(e) {
     //获取鼠标针对可视区域的位置
@@ -86,4 +60,4 @@
       left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0
     };
   }
-})();
+})()
